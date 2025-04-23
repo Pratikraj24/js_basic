b058edd4e35ffcc9cc212f7de8811e1f6eb2f28c
Pratikraj24 <139611799+Pratikraj24@users.noreply.github.com>
2025-04-23 19:50:34 +0000

    lecture_05

--- 01_basics/first_code.js
+++ 01_basics/first_code.js
@@ -1,1 +1,10 @@
 console.log("pratik")
+const accnt_no=12217
+let accnt_mail="pratik24@gmail.com"
+var accnt_pass="#javascript24"
+accnt_city="jaipur"
+accnt_mail="updated24@gmail.com"
+accnt_pass="#update24"
+accnt_city="patna"
+console.table([accnt_no,accnt_mail,accnt_pass,accnt_city])
+
---
+++ 01_basics/lecture_05.js
@@ -0,0 +1,10 @@
+"use strict"// treat all syntax as newer version of js;
+// alert("hello"); // won't give any o/p bcz we are using node js not browser;
+let state;  // o/p =undefined
+let eg2=true; // o/p will  be boolen
+let eg3=123;   // o/p will be number
+let eg4="pratik";    //string
+let eg5=null;     //object
+
+console.log(typeof [state,eg2,eg3,eg4]);
+
