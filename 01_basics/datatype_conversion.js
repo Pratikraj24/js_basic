1c74baf0e7ac8e637999342d11145a9878af6cd2
Pratikraj24 <139611799+Pratikraj24@users.noreply.github.com>
2025-04-24 19:25:22 +0000

    conversion of datatype

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,42 @@
+// let score= "33";
+// console.log(typeof score);
+// let converted=Number(score);
+// console.log(typeof converted);
+// console.log(converted);
+
+
+/* so initially score="33" was a string nut we changed  their datatype to
+ number by using-> Number(score); and store in xyz
+*/
+
+/*
+let marks=null;   
+let newMarks=Number(marks);
+console.log(typeof newMarks);  //o/p-> number
+console.log(newMarks);      //o/p-> 0
+*/
+
+
+/*
+let marks=undefined;   
+let newMarks=Number(marks);
+console.log(typeof newMarks);  //o/p-> number
+console.log(newMarks);      //o/p-> NaN
+*/
+
+// #converting into Boolean
+
+/*let isLoged_in=1;
+let converted=Boolean(isLoged_in);
+console.log(typeof converted);   //boolean
+console.log(converted);           // true
+
+ 1->true     0-> false    ""-> false      "string"-> */
+
+//  #converting into String
+/*let user_name=123;
+let converted=String(user_name);
+console.log(typeof converted);    //string
+console.log(converted);    //123
+*/
+let 
