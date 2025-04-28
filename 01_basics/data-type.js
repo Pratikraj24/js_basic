bd0e9d7d7ce807358b47c62167c0da4ce9cf591c
Pratikraj24 <139611799+Pratikraj24@users.noreply.github.com>
2025-04-28 19:22:09 +0000

    data-type summary

---
+++ 01_basics/data-type.js
@@ -0,0 +1,43 @@
+/* primitive data type
+ 7 types: string, number, boolean ,null, undefined ,symbol, BigInt;
+ eg:
+ const value=100;
+ const name="pratik";
+ const temperture=null;
+ const new_id=undefined  or  let new_id;
+ const isloggedin=true;
+ const id=symbol('123');
+ const fakeid=symbol('123');
+ console.log(id===fakeid);  o/p =false;
+ const bigint=12356476464454653757n   // placing n after no recognise them as bigint;
+
+ */
+
+ /* reference (non-primitive data type)
+ array, object ,function
+
+ example:
+ array->  const heroes=["ironman","shaktiman","batman","deadpool","thanos"];
+ array consist in square bracket
+ object-> let myObj={
+            Name:"pratik",
+            age:22,
+            usn:1mv21ec068
+  }
+            object consist of curly bracket;
+            value inside an object can be  anything
+           ( like number, string, boolean, array, function, itself object.)
+
+           // decleration of function
+           function(){
+           console.log("hello world");
+           }
+
+ */
+// const heroes=["pratik","raj","madan"];
+// const myfunction=function(){
+//     console.log("hello world");
+//     }
+// console.log(typeof heroes); op=object
+// console.log(typeof myfunction);   op= function
+
