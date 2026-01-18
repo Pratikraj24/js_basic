ef490cc02515015e5957ebd96297223c1c88dd9a
Pratikraj24 <139611799+Pratikraj24@users.noreply.github.com>
2026-01-18 13:44:30 +0000

    datatype&ecma

---
+++ datatype_ecma.js
@@ -0,0 +1,16 @@
+let makichu="bhosarpappu";
+// datatypes;
+/* 
+number>> 2to the power 53;
+bigint>> basically used in trading , stock marketing (very large value);
+string>> no need to explain ;
+boolean >> no need ;
+null>> standalone value;
+    eg.let syudent=null;
+    explanation:you request a server for temperature but because of technical issue you didnt rcv the temp , so in that case you will print null (not 0) ;
+undefined>> when we doesnt provide value but defined the datatype;
+    eg. let name;    
+symbol>> unique;
+*/
+// console.log(typeof null) o/p= object;
+
